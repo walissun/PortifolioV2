f5eb2c4993f3858ac9a47af6e08ae8961417b0df
walissun <rwalisson972@gmail.com>
2024-06-09 23:56:10 -0300

    atulização pequena

--- js/achiviment.js
+++ js/achiviment.js
@@ -9,7 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
    
     setTimeout(function() {
-        creditoscontainer.classList.add("esconder");
-        princial.classList.remove("esconder");
-    }, 10000); // Match the animation duration (10 seconds)
+        creditoscontainer.style.opacity = '0';
+        setTimeout(function() {
+            creditoscontainer.classList.add("esconder");
+            princial.style.opacity = '1';
+            princial.classList.remove("esconder");
+        }, 1000); // Match the transition duration (1 second)
+    }, 5000); // Match the animation duration (5 seconds)
 });
