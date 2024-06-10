27e716478c46573b1f803d76d7190a638cff4074
walissun <rwalisson972@gmail.com>
2024-06-10 00:18:39 -0300

    pequena alteração no js

--- js/esconder menu.js
+++ js/esconder menu.js
@@ -1,13 +1,9 @@
-// Variável para selecionar o botão de navegação e o menu
+
 const toggleNavButton = document.getElementById('toggleNav');
 const navMenu = document.getElementById('navMenu');
-// Função para alternar a visibilidade do menu
+
 function toggleMenu() {
-    if (navMenu.style.display === 'block') {
-        navMenu.style.display = 'none';
-    } else {
-        navMenu.style.display = 'block';
-    }
+    navMenu.classList.toggle('hidden');
 }
-// Adiciona um ouvinte de evento para quando o botão de navegação é clicado
+
 toggleNavButton.addEventListener('click', toggleMenu);
